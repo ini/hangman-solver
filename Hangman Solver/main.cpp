3cd22b40f459cfb0e48fac60a51582c0d31719de
Ini Oguntola <Ini@Inis-MacBook-Pro-2.local>
2015-08-08 06:37:40 -0400

    Initial Commit

---
+++ Hangman Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Hangman Solver
+//
+//  Created by Ini on 8/8/15.
+//  Copyright (c) 2015 Insi Productions. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
